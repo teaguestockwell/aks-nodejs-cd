b597cbb1a32508e73d0c7467282f9336899f47c7
teagueStockwell <tsAppDevelopment@gmail.com>
2022-07-15 00:44:41 -0700

    Add jwt example

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,27 @@
-import express, {Application} from 'express'
-import compression from 'compression'
-import responseTime from 'response-time'
+import compression from "compression";
+import express, { Application } from "express";
+import responseTime from "response-time";
+import { issueSig, withAuth } from "./with-auth";
 
-const app: Application = express()
+const app: Application = express();
 
-app.disable('x-powered-by')
+app.disable("x-powered-by");
 
-app.use(express.json())
-app.use(responseTime())
-app.use(compression())
+app.use(express.json());
+app.use(responseTime());
+app.use(compression());
 
-app.get('/', (_req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (_req, res) => {
+  res.status(200).json('hi mom');
+});
 
-app.listen(process.env.PORT)
-export default app
+app.get("/protected", withAuth, (req, res) => {
+  res.status(200).json((req as any).tok);
+});
+
+app.get("/get-sig", (_req, res) => {
+  res.status(200).json(issueSig({ msg: 'hi mom'}));
+});
+
+app.listen(process.env.PORT);
+export default app;
---
+++ src/with-auth.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express";
+import { sign, verify } from "jsonwebtoken";
+
+const { secret } = process.env;
+
+export const withAuth = (req: Request, res: Response, next: NextFunction) => {
+  const tok = req.headers["authorization"];
+
+  if (!tok || typeof tok !== "string") {
+    return res.status(401).send({ error: "authorization header required" });
+  }
+
+  try {
+    (req as any).tok = verify(tok, secret!);
+    next();
+    return;
+  } catch {}
+
+  return res.status(403).send({ error: "invalid authorization token" });
+};
+
+export const issueSig = (meta: any) => {
+  return sign(meta, secret!, {
+    expiresIn: 60 * 60 * 24 * 7,
+  });
+};
