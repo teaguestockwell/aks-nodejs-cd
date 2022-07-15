57eefee61767d486876ce46f0a28275ae740199c
teagueStockwell <tsAppDevelopment@gmail.com>
2022-07-14 21:36:47 -0700

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'production',
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'server.js',
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          'ts-loader',
+        ]
+      }
+    ]
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+        terserOptions: {
+          format: {
+            comments: false,
+          },
+        },
+      }),
+    ],
+  },
+}
